a4c4be34b0e79bbaa9eb5d897ba4e1ea1942674f
quochung9920 <quochung9920@gmail.com>
2022-07-04 18:23:33 +0700

    RequestParam and RequestMapping

--- springmvcdemo/src/main/java/com/mycompany/controllers/HomeController.java
+++ springmvcdemo/src/main/java/com/mycompany/controllers/HomeController.java
@@ -1,14 +1,33 @@
 package com.mycompany.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index(Model model) {
-        model.addAttribute("name", "Hưng"); 
+    public String index(Model model, 
+        @RequestParam Map<String, String> params) {
+        String firstName = params.get("first_name");
+        String lastName = params.get("last_name");
+        if(firstName != null && lastName != null) {
+            model.addAttribute("name", String.format("%s %s", firstName, lastName));
+        }
+        else{
+            model.addAttribute("name", "Hưng"); 
+        }
         return "index";
+    }
+
+    @RequestMapping("/hello/{name}")
+    public String hello(Model model, 
+        @PathVariable(value = "name") String name) {
+        model.addAttribute("message", "Welcome " + name);
+        return "hello";
     }
 }
---
+++ springmvcdemo/target/springmvcdemo-1.0-SNAPSHOT/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : hello
+    Created on : 4 Jul 2022, 17:35:18
+    Author     : quoch
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Welcome</title>
+    </head>
+    <body>
+        <h1>${message}</h1>
+    </body>
+</html>
